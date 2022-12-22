7ab8797fd3c157101623ec252ba7672ad3d64669
gab1144 <gabry.rinciari04@gmail.com>
2022-12-22 18:00:41 +0100

    [change]blue to primarycolor, immagini link footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -72,19 +72,19 @@
           <h4>Follow us</h4>
           <ul>
             <li>
-              <a href="#"></a>
+              <a href="#"><img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt=""></a>
             </li>
             <li>
-                <a href="#"></a>
+                <a href="#"><img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt=""></a>
             </li>
             <li>
-                <a href="#"></a>
+                <a href="#"><img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt=""></a>
             </li>
             <li>
-                <a href="#"></a>
+                <a href="#"><img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt=""></a>
             </li>
             <li>
-                <a href="#"></a>
+                <a href="#"><img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt=""></a>
             </li>
           </ul>
         </div>
